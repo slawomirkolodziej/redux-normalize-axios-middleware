30340bde83521b578d9444c72e7701c6a545e7a3
Sławomir Kołodziej <slawomir.kolodziej@netguru.pl>
2018-05-14 16:38:05 +0200

    Clean code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import { normalize } from 'normalizr';
 import { has, mapValues, keys } from 'lodash';
 
-export default store => next => action => {
-  if (has(action, 'payload.data') && has(action, 'meta.previousAction.schema')) {
-    const normalizedData = normalize(action.payload.data, action.meta.previousAction.schema);
-    const normalizedDataWithIds = mapValues(normalizedData.entities, entity => (
-      { items: entity, allIds: keys(entity) }
-    ));
-    return next({
-      ...action,
-      payload: {
-        ...action.payload,
-        data: normalizedDataWithIds,
-      }
-    });
-  }
+const shouldNormalizeAction = action => (
+  has(action, 'payload.data') && has(action, 'meta.previousAction.schema')
+);
 
-  return next(action);
+const appendAllIds = data => mapValues(data.entities, entity => (
+  { items: entity, allIds: keys(entity) }
+));
+
+export default store => next => action => {
+  if(!shouldNormalizeAction(action)) return next(action);
+
+  const normalizedData = normalize(action.payload.data, action.meta.previousAction.schema);
+  const normalizedDataWithAllIds = appendAllIds(normalizedData);
+  return next({
+    ...action,
+    payload: {
+      ...action.payload,
+      data: normalizedDataWithAllIds,
+    }
+  });
 };
