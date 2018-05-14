5e03881d8a06d52a6becf3a0039fd2bc6a286d3e
Sławomir Kołodziej <slawomir.kolodziej@netguru.pl>
2018-05-14 16:29:18 +0200

    Add tests

---
+++ src/index.test.js
@@ -0,0 +1,109 @@
+import configureStore from 'redux-mock-store';
+import { schema } from 'normalizr';
+
+import normalizeAxiosMiddleware from './';
+
+describe('normalize middleware', () => {
+  const mockStore = configureStore([normalizeAxiosMiddleware]);
+  const store = mockStore();
+
+  afterEach(() => {
+    store.clearActions();
+  });
+
+  const response = {
+    data: [
+      {
+        id: 1,
+        title: 'Article 1',
+        author: {
+          id: 10,
+          name: 'John',
+        },
+      },
+      {
+        id: 2,
+        title: 'Article 2',
+        author: {
+          id: 20,
+          name: 'Alex',
+        },
+      },
+    ],
+  };
+
+  const authorSchema = new schema.Entity('authors');
+  const articlesSchema = [new schema.Entity('articles', {
+    author: authorSchema,
+  })];
+
+  const action = {
+    type: 'success',
+    payload: response,
+    meta: {
+      previousAction: {
+        schema: articlesSchema,
+      },
+    },
+  };
+
+  const normalizedArticles = {
+    1: {
+      id: 1,
+      title: 'Article 1',
+      author: 10,
+    },
+    2: {
+      id: 2,
+      title: 'Article 2',
+      author: 20,
+    },
+  };
+
+  const normalizedAuthors =  {
+    10: {
+      id: 10,
+      name: 'John',
+    },
+    20: {
+      id: 20,
+      name: 'Alex',
+    },
+  };
+
+  it('should normalize items according to schema', () => {
+    store.dispatch(action);
+    expect(store.getActions()[0].payload.data.articles.items).toEqual(normalizedArticles);
+    expect(store.getActions()[0].payload.data.authors.items).toEqual(normalizedAuthors);
+  });
+
+  it('should include ids of items', () => {
+    store.dispatch(action);
+    expect(store.getActions()[0].payload.data.articles.allIds).toEqual(['1', '2']);
+    expect(store.getActions()[0].payload.data.authors.allIds).toEqual(['10', '20']);
+  });
+
+  it('should not try to normalize data if no schema provided', () => {
+    const actionWithoutSchema = {
+      type: 'success',
+      payload: response,
+    };
+
+    store.dispatch(actionWithoutSchema);
+    expect(store.getActions()[0]).toEqual(actionWithoutSchema);
+  });
+
+  it('should not try to normalize data if no payload provided', () => {
+    const actionWithoutSchema = {
+      type: 'success',
+      meta: {
+        previousAction: {
+          schema: articlesSchema,
+        },
+      },
+    };
+
+    store.dispatch(actionWithoutSchema);
+    expect(store.getActions()[0]).toEqual(actionWithoutSchema);
+  });
+});
