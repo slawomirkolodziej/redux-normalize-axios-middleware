165cc57c29ee75497bdb3949bec01b1448a99642
Sławomir Kołodziej <slawomir.kolodziej@netguru.pl>
2018-05-15 17:35:33 +0200

    Fix lodash imports, change build filename

--- dist/index.js
+++ dist/index.js
@@ -8,15 +8,35 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
 var _normalizr = require('normalizr');
 
-var _lodash = require('lodash');
+var _get = require('lodash/get');
+
+var _get2 = _interopRequireDefault(_get);
+
+var _has = require('lodash/has');
+
+var _has2 = _interopRequireDefault(_has);
+
+var _keys = require('lodash/keys');
+
+var _keys2 = _interopRequireDefault(_keys);
+
+var _map = require('lodash/map');
+
+var _map2 = _interopRequireDefault(_map);
+
+var _mapValues = require('lodash/mapValues');
+
+var _mapValues2 = _interopRequireDefault(_mapValues);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var shouldNormalizeAction = function shouldNormalizeAction(action) {
-  return (0, _lodash.has)(action, 'payload.data') && (0, _lodash.has)(action, 'meta.previousAction.normalize.schema');
+  return (0, _has2.default)(action, 'payload.data') && (0, _has2.default)(action, 'meta.previousAction.normalize.schema');
 };
 
 var appendAllIds = function appendAllIds(data) {
-  return (0, _lodash.mapValues)(data.entities, function (entity) {
-    return { items: entity, allIds: (0, _lodash.map)(entity, function (item) {
+  return (0, _mapValues2.default)(data.entities, function (entity) {
+    return { items: entity, allIds: (0, _map2.default)(entity, function (item) {
         return item.id;
       }) };
   });
@@ -29,9 +49,9 @@ exports.default = function (store) {
 
       var data = action.payload.data;
       var schema = action.meta.previousAction.normalize.schema;
-      var path = (0, _lodash.get)(action.meta.previousAction.normalize, 'path');
+      var path = (0, _get2.default)(action.meta.previousAction.normalize, 'path');
 
-      var requestData = path ? (0, _lodash.get)(data, path) : data;
+      var requestData = path ? (0, _get2.default)(data, path) : data;
       var normalizedData = (0, _normalizr.normalize)(requestData, schema);
       var normalizedDataWithAllIds = appendAllIds(normalizedData);
       return next(_extends({}, action, {
